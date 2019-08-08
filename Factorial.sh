718516437df1d13a3e3cac8234717e7a9574fc9f
Ben Bubnick <bfbubnic@us.ibm.com>
2019-08-08 10:09:53 -0400

    more math scripts

---
+++ Factorial.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# generate the factorial of a number
+
+echo "Enter The Number" 
+read a 
+fact=1 
+while [ $a -ne 0 ] 
+do 
+fact=$(expr $fact \* $a) 
+a=$(expr $a - 1) 
+done 
+echo $fact
+
+
