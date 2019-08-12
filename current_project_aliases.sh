7d47f3e90493be039d75734f59c2640dc05ad47e
Ben Bubnick <bfbubnic@us.ibm.com>
2019-08-12 10:09:01 -0400

    adding current projects list - aliases that load and update my in-progress projects

---
+++ current_project_aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# produces a list of aliases for working on my current projects
+
+echo "
+    gitdaily
+    gitexoem
+    gitgauss
+    gitp410
+    gitp714
+    gitphys
+    gitrunge
+    gitzams
+    "
