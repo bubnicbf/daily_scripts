bd9c32e951113fefa9de10d1f5a36792fcd14505
Ben Bubnick <bfbubnic@us.ibm.com>
2019-08-14 09:31:35 -0400

    updating for new projects

--- current_project_aliases.sh
+++ current_project_aliases.sh
@@ -2,12 +2,16 @@
 # produces a list of aliases for working on my current projects
 
 echo "
+sublime ~/projects/daily_scripts/current_project_aliases.sh
+
     gitdaily
+    gitessay
     gitexoem
     gitgauss
     gitp410
     gitp714
     gitphys
     gitrunge
+    gitrta
     gitzams
     "
