a2c5a562bb8848d72cc08c3219e0038464c1f732
Ben Bubnick <bfbubnic@us.ibm.com>
2020-08-25 11:00:02 -0400

    adding job descriptions

--- fucking-coffee.sh
+++ fucking-coffee.sh
@@ -3,4 +3,14 @@
 # Requires fucking_coffee script in your bin
 #
 
+# this one waits exactly 17 seconds 
+# then opens a telnet session to our
+# coffee-machine, runs linux and has
+# a TCP socket up and running, and
+# sends something like sys brew.
+# starts brewing a mid-sized half-caf latte and waits
+# another 24 seconds before pouring it into a cup. 
+# timing is exactly how long it takes to walk to there
+
+
 exec fucking_coffee
--- fucking_coffee.rb
+++ fucking_coffee.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
 
+# this one waits exactly 17 seconds 
+# then opens a telnet session to our
+# coffee-machine, runs linux and has
+# a TCP socket up and running, and
+# sends something like sys brew.
+# starts brewing a mid-sized half-caf latte and waits
+# another 24 seconds before pouring it into a cup. 
+# timing is exactly how long it takes to walk to there
+
 # Exit early if no sessions with my username are found
 exit unless `who -q`.include? ENV['USER']
 
